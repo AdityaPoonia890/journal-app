5c2e913a0c1e8e4366f1dd33f66987e41ad21ec8
AdityaPoonia <145849577+AdityaPoonia890@users.noreply.github.com>
2025-01-28 19:47:44 +0530

    send mail

---
+++ src/main/java/com/aditya/journalApp/service/EmailService.java
@@ -0,0 +1,30 @@
+package com.aditya.journalApp.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class EmailService {
+
+    @Autowired
+    JavaMailSender javaMailSender;
+
+    public void sendMail(String to, String subject, String body) {
+
+        try{
+            SimpleMailMessage mail = new SimpleMailMessage();
+            mail.setTo(to);
+            mail.setText(body);
+            mail.setSubject(subject);
+
+        } catch (Exception e) {
+            log.error("error while sending mail," ,e);
+        }
+
+
+    }
+}

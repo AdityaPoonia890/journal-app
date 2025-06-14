1fc2a2fe7e4b38ab90fd407e34fa360279921212
AdityaPoonia <145849577+AdityaPoonia890@users.noreply.github.com>
2025-06-15 03:35:02 +0530

    more changes

--- src/main/java/com/aditya/journalApp/repository/UserRepo.java
+++ src/main/java/com/aditya/journalApp/repository/UserRepo.java
@@ -1,13 +1,13 @@
-//package com.aditya.journalApp.repository;
-//
-//import com.aditya.journalApp.entity.User;
-//import org.bson.types.ObjectId;
-//import org.springframework.data.mongodb.repository.MongoRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface UserRepo extends MongoRepository<User, Integer> {
-//    User findByUserName(String userName);
-//
-//    void deleteByUserName(String userName);
-//}
+package com.aditya.journalApp.repository;
+
+import com.aditya.journalApp.entity.User;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends MongoRepository<User, Integer> {
+    User findByUserName(String userName);
+
+    void deleteByUserName(String userName);
+}

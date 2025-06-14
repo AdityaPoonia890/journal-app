d7fde70a657313bd3f5e4057d66857d7aa865864
AdityaPoonia <145849577+AdityaPoonia890@users.noreply.github.com>
2025-06-15 03:30:16 +0530

    more changes

--- src/main/java/com/aditya/journalApp/cache/AppCache.java
+++ src/main/java/com/aditya/journalApp/cache/AppCache.java
@@ -1,34 +1,34 @@
-package com.aditya.journalApp.cache;
-
-import com.aditya.journalApp.entity.ConfigJournalAppEntity;
-import com.aditya.journalApp.repository.ConfigJournalAppRepo;
-import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-@Component
-public class AppCache {
-
-    @Autowired
-    ConfigJournalAppRepo configJournalAppRepo;
-
-    public HashMap<String, String> appCacheMap;
-
-    @PostConstruct
-    public void init() {
-
-        appCacheMap = new HashMap<>();
-
-        List<ConfigJournalAppEntity> all = configJournalAppRepo.findAll();
-
-        for (ConfigJournalAppEntity configJournalAppEntity: all) {
-            appCacheMap.put(configJournalAppEntity.getKey(), configJournalAppEntity.getValue());
-        }
-    }
-
-}
+//package com.aditya.journalApp.cache;
+//
+//import com.aditya.journalApp.entity.ConfigJournalAppEntity;
+//import com.aditya.journalApp.repository.ConfigJournalAppRepo;
+//import jakarta.annotation.PostConstruct;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Component;
+//
+//import java.util.ArrayList;
+//import java.util.HashMap;
+//import java.util.List;
+//import java.util.Map;
+//
+//@Component
+//public class AppCache {
+//
+//    @Autowired
+//    ConfigJournalAppRepo configJournalAppRepo;
+//
+//    public HashMap<String, String> appCacheMap;
+//
+//    @PostConstruct
+//    public void init() {
+//
+//        appCacheMap = new HashMap<>();
+//
+//        List<ConfigJournalAppEntity> all = configJournalAppRepo.findAll();
+//
+//        for (ConfigJournalAppEntity configJournalAppEntity: all) {
+//            appCacheMap.put(configJournalAppEntity.getKey(), configJournalAppEntity.getValue());
+//        }
+//    }
+//
+//}
--- src/main/java/com/aditya/journalApp/entity/ConfigJournalAppEntity.java
+++ src/main/java/com/aditya/journalApp/entity/ConfigJournalAppEntity.java
@@ -1,14 +1,14 @@
-package com.aditya.journalApp.entity;
-
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.springframework.data.mongodb.core.mapping.Document;
-
-@Data
-@NoArgsConstructor
-@Document(collection = "config_journal_app")
-public class ConfigJournalAppEntity {
-    private String key;
-    private String value;
-
-}
+//package com.aditya.journalApp.entity;
+//
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//import org.springframework.data.mongodb.core.mapping.Document;
+//
+//@Data
+//@NoArgsConstructor
+//@Document(collection = "config_journal_app")
+//public class ConfigJournalAppEntity {
+//    private String key;
+//    private String value;
+//
+//}

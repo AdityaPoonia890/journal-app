f6fa2cefe231fe45cc3ab9c4d046d04fa74ec5e0
AdityaPoonia <145849577+AdityaPoonia890@users.noreply.github.com>
2025-07-11 17:37:01 +0530

    added endpoint of uptime calls

---
+++ src/main/java/com/aditya/journalApp/controller/HealthController.java
@@ -0,0 +1,15 @@
+package com.aditya.journalApp.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/health")
+public class HealthController {
+
+    @GetMapping
+    public static ResponseEntity<HttpStatus> healthCheck() {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
